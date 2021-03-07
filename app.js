0665a1e85de3c0e5dcb516f29d98b7a598aefce2
Mariia Paraketsova <paraketsova@gmail.com>
2021-03-07 21:04:03 +0100

    Add response JSON array_test

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname + '/public/html/index.html'));
 });
 
-
+app.get('/api/getChannels', function (request, response) {
+  const channels = ['aaa', 'bbb', 'ccc'];
+  response.json(channels);
+});
 
 app.listen(3000);
